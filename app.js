ded62d9abe01830a7b70f5d9e83b3b35684f2e02
3petersd <pauline.von.petersdorff@studium.uni-hamburg.de>
2026-09-16 18:40:12 +0200

    Task-Type bei Immobilienaufgaben geändert (Image + Table)

--- app.js
+++ app.js
@@ -525,7 +525,7 @@ const taskGroups = [
             "u.a. Baujahr, Wohnfläche und Lage. \n\n" +
             "Ihre Aufgabe besteht darin, den gelisteten Kaufpreis der Immobilie einzuschätzen.",
 
-        type: "table",
+        type: "image_table",
 
         prompt:
             "Betrachten Sie die folgenden Informationen. " +
